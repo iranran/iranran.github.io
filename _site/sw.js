2ebf352fddc9689f0b67c5b41e220234f9090c66
liwei <wei.li@bhex.io>
2019-02-24 14:33:56 +0800

    new add

--- _site/sw.js
+++ _site/sw.js
@@ -1,8 +1,8 @@
-const version = '20190224143032';
+const version = '20190224143256';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/hello-rld90-hello","/hello-world","/about-java-reflection","/about-linux-free","/about","/categories","/blog/","/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","/assets/logo.svg", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/about-java-reflection","/about-linux-free","/about","/categories","/blog/","/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","/assets/logo.svg", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
